afad617a8447f390c96ae0e12c38dbfd0c0c3104
CoderGuy614 <jlutz.110@gmail.com>
2020-02-21 20:36:10 +0800

    reversed data in data.json

--- errorHandlers.js
+++ errorHandlers.js
@@ -1,16 +1,15 @@
 // Error handler for handling non-existent routes
-const errorHandler404 = ((req, res, next) => {
-    console.log('Handling 404 error');
-    const err = new Error('err');
-    err.status = 404;
-    err.message = 'Oopsie!  This page does not exist!';
-    next(err);
-  });
-  
-  // Global error handler
- const handleGlobalError = ((err, req, res, next) => {
-    res.locals.error = err
-    res.render('error', err);
-  });
+const errorHandler404 = (req, res, next) => {
+  const err = new Error("err");
+  err.status = 404;
+  err.message = "Oopsie!  This page does not exist!";
+  next(err);
+};
 
-  module.exports = {errorHandler404, handleGlobalError};
+// Global error handler
+const handleGlobalError = (err, req, res, next) => {
+  res.locals.error = err;
+  res.render("error", err);
+};
+
+module.exports = { errorHandler404, handleGlobalError };
--- routes.js
+++ routes.js
@@ -1,26 +1,25 @@
 // Importing express and the data file
-const express = require('express')
-const router = express.Router()
-const  { projects }  = require('./data.json')
+const express = require("express");
+const router = express.Router();
+const { projects } = require("./datarev.json");
 //Renders the index page
-router.get('/', function (req, res) {
-    res.render('index', { projects })
-  })
+router.get("/", function(req, res) {
+  res.render("index", { projects: projects.reverse() });
+});
 // Renders the about page template
-router.get('/about', function (req, res) {
-    res.render('about')
-  })
+router.get("/about", function(req, res) {
+  res.render("about");
+});
 // Renders the pug template for each project
-  router.get('/projects/:id', function (req, res) {
-    const projectId = req.params.id;
-    const project = projects.find( ({ id })  => id === +projectId);
-    
-    if(project){
-    res.render('project', { project })
-    } else {
-        res.sendStatus(404);
-    }
-  })
+router.get("/projects/:id", function(req, res) {
+  const projectId = req.params.id;
+  const project = projects.find(({ id }) => id === +projectId);
+
+  if (project) {
+    res.render("project", { project });
+  } else {
+    res.sendStatus(404);
+  }
+});
 
 module.exports = router;
-
